2f97d105e841b2c5769d40d62a0a30073cf20ca0
derekvawdrey <derek.vawdrey@gmail.com>
2023-07-14 12:46:44 -0600

    Adding in accuracy and stuff

--- src/Model/DrupalGPTMessage.php
+++ src/Model/DrupalGPTMessage.php
@@ -7,25 +7,33 @@ use Drupal\drupal_gpt\Controller\ApiController;
 class DrupalGPTMessage {
     protected ApiController $api_controller;
 
+    
     // The message and context of the string
     protected string $message;
     protected string $context;
+    protected string $message_author;
 
     // Determines if the message is a previous message or not
     protected boolean $already_processed;
+    protected boolean $ai_response;
     
     // Determines if the previous message was accuracy
     protected float $accuracy;
 
-    function __construct($message, $context = "", $already_processed = false, $ai_response = true){
+    function __construct($message, $context = "", $already_processed = false, $ai_response = true, $accuarcy = 1){
         $this->api_controller = new ApiController();
         $this->message = $message;
         $this->context = $context;
         $this->already_processed = $already_processed;
+        $this->ai_response = $ai_response;
+
+        $this->message_author = "system";
+        if($this->ai_response) $this->message_author = "assistant";
+
         if(!$already_processed && $ai_response){
             $this->accuracy = $this->verifyAccuracy($this->message, $this->context);
-        }else{
-            $this->accuracy = 0;
+        }else {
+            $this->accuracy = $accuarcy;
         }
     }
 
@@ -51,6 +59,13 @@ class DrupalGPTMessage {
 
      public function getMessage(){
         return $this->message;
+     }
+     public function getMessageAuthor(){
+        return $this->message_author;
+     }
+
+     public function getContext(){
+        return $this->context;
      }
 
 
--- src/Model/DrupalGPTSession.php
+++ src/Model/DrupalGPTSession.php
@@ -18,7 +18,7 @@ class DrupalGPTSession {
     function __contruct($session_id, $session_node){
         $this->session_id = $session_id;
         $this->session_node = $session_node;
-
+        $this->generateMessageArray();
     }
     
     public function addMessage($message, $context = "", $already_processed = false, $ai_response = true){
@@ -28,16 +28,43 @@ class DrupalGPTSession {
         return $drupal_gpt_message;
     }
 
+    /**
+     * 
+     * Load from node to fill message_chain
+     * 
+     */
     public function generateMessageArray(){
         $this->session_node;
     }
 
     private function loadMessagesFromNode(){
-        
+        $json = json_decode($this->session_node->get("body"));
+        foreach($json as $message){
+            $accuracy = $json["accuracy"];
+            $context = $json["context"];
+            $ai_response = false;
+            if($json["role"]=="assistant") $ai_response = true;
+            $this->message_chain[] = new DrupalGPTMessage($json["content"], "", true, true, $accuracy);
+        }
     }
 
     private function saveToNode(){
+        $json = [
+            "messages"=>[]
+        ];
+        foreach($this->message_chain as $message){
+            $message_json = [
+                "accuracy"=>$message->getAccuracy(),
+                "content"=>$message->getMessage(),
+                "context"=>$message->getContext(),
+                "role"=>$message->getMessageAuthor(),
+            ];
 
+
+            $json["messages"][] = $message_json;
+        }
+        $this->session_node->set("body",json_encode($json));
+        $this->session_node->save();
     }
 
     
